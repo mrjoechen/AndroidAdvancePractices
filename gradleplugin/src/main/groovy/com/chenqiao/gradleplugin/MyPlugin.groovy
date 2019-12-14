134bcec26795e91afc6edcb10de975b3537b0b8f
chenqiao <mrjoechen@qq.com>
2019-12-14 10:34:26 +0800

    Custom gradle plugin

---
+++ gradleplugin/src/main/groovy/com/chenqiao/gradleplugin/MyPlugin.groovy
@@ -0,0 +1,15 @@
+package com.chenqiao.gradleplugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+
+class MyPlugin implements Plugin<Project> {
+
+    void apply(Project project) {
+        System.out.println("========================")
+        System.out.println("hello gradle plugin!")
+        System.out.println("========================")
+    }
+}
