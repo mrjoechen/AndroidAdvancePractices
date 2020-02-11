7c8891142609eff97df6a980883d7440c5fc9739
chenqiao <mrjoechen@qq.com>
2020-02-12 02:47:16 +0800

    customeview

--- customview/src/main/java/com/chenqiao/customview/XferModeView.java
+++ customview/src/main/java/com/chenqiao/customview/XferModeView.java
@@ -23,9 +23,9 @@ public class XferModeView extends View {
 
     private Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    private Bitmap circleBitmap = Bitmap.createBitmap(300,300 , Config.ARGB_8888);
+    private Bitmap circleBitmap = Bitmap.createBitmap(500,500 , Config.ARGB_8888);
 
-    private Bitmap rectBitmap = Bitmap.createBitmap(300,300 , Config.ARGB_8888);
+    private Bitmap rectBitmap = Bitmap.createBitmap(500,500 , Config.ARGB_8888);
 
     private Canvas rectCanvas = new Canvas(rectBitmap);
     private Canvas circleCanvas = new Canvas(circleBitmap);
@@ -33,36 +33,114 @@ public class XferModeView extends View {
 
     public XferModeView(Context context) {
         super(context);
+        initView();
     }
 
     public XferModeView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initView();
     }
 
     public XferModeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        bounds.set(0, 0, 300, 300);
 
-        mPaint.setColor(Color.BLUE);
-        rectCanvas.drawCircle(150, 50, 50, mPaint);
+//        bounds.set(0, 0, getMeasuredWidth(), getMeasuredHeight());
+//        mPaint.setStyle(Paint.Style.STROKE);
+//        canvas.drawRect(bounds, mPaint);
+//
+//////////////////////
+//        mPaint.setStyle(Paint.Style.FILL);
+//
+//        mPaint.setColor(Color.BLUE);
+//        rectCanvas.drawRect(0, 0, 200, 300, mPaint);
+//
+//        mPaint.setColor(Color.RED);
+//        circleCanvas.drawCircle(200, 200, 200, mPaint);
+//
+//
+//        int i = canvas.saveLayer(bounds, mPaint);
+//        canvas.drawBitmap(circleBitmap, 0, 0, mPaint);
+//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+//        canvas.drawBitmap(rectBitmap, 0, 0, mPaint);
+//        canvas.restoreToCount(i);
+//        mPaint.setXfermode(null);
 
-        mPaint.setColor(Color.RED);
-        circleCanvas.drawRect(bounds, mPaint);
 
 
-        int i = canvas.saveLayer(bounds, mPaint);
-        canvas.drawBitmap(circleBitmap, 0, 0, mPaint);
-        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(rectBitmap, 0, 0, mPaint);
-        canvas.restoreToCount(i);
-        mPaint.setXfermode(null);
+        ////////////////////////
+//        mPaint.setStyle(Paint.Style.FILL);
+//
+//        mPaint.setColor(Color.BLUE);
+//
+//        int i = canvas.saveLayer(bounds, mPaint);
+//
+//
+//        //画方
+//        RectF rectF = new RectF();
+//        rectF.set(0, getMeasuredHeight()/2, getMeasuredWidth()/2, getMeasuredHeight());
+//        canvas.drawRect(rectF, mPaint);
+//
+//        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+//
+//        //画圆
+//        mPaint.setColor(Color.RED);
+//        canvas.drawCircle(getMeasuredWidth()/2, getMeasuredHeight()/2, getMeasuredWidth()/3, mPaint);
+//
+//        canvas.restoreToCount(i);
+//        mPaint.setXfermode(null);
 
 
+        Bitmap rectBitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
+
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        Canvas canvas1 = new Canvas(rectBitmap);
+        paint.setColor(Color.BLUE);
+        canvas1.drawRoundRect(0, 0, rectBitmap.getWidth()/2, rectBitmap.getHeight()/2,50, 50, paint);
+
+        out = Bitmap.createBitmap(rectBitmap.getWidth(), rectBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        Canvas canvasout = new Canvas(out);
+        //DST
+        paint.setColor(Color.RED);
+        canvasout.drawCircle(rectBitmap.getWidth()/2, rectBitmap.getHeight()/2, 200, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+//        //SRC
+        canvasout.drawBitmap(rectBitmap, 0, 0, paint);
+        paint.setXfermode(null);
+
+        canvas.drawBitmap(out, 0, 0, null);
+    }
+
+    private Bitmap out;
+
+    private void initView(){
+
+
+//        Bitmap rectBitmap = Bitmap.createBitmap(500, 500, Bitmap.Config.ARGB_8888);
+//
+//        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+//
+//        Canvas canvas1 = new Canvas(rectBitmap);
+//        paint.setColor(Color.BLUE);
+//        canvas1.drawRoundRect(0, 0, rectBitmap.getWidth()/2, rectBitmap.getHeight()/2,50, 50, paint);
+//
+//        out = Bitmap.createBitmap(rectBitmap.getWidth(), rectBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+//
+//        Canvas canvas = new Canvas(out);
+//        //DST
+//        paint.setColor(Color.RED);
+//        canvas.drawCircle(rectBitmap.getWidth()/2, rectBitmap.getHeight()/2, 200, paint);
+//        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+////        //SRC
+//        canvas.drawBitmap(rectBitmap, 0, 0, paint);
+//        paint.setXfermode(null);
 
     }
 }
